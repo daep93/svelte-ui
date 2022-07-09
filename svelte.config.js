5aee53ccdd1fb75f36ba1d9dbd84b410748e3929
dahyun <deahyun@thevc.kr>
2022-07-10 02:30:13 +0900

    feat:alias 등록

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
-import sveltePreprocess from "svelte-preprocess";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess(),
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+      $assets: path.resolve("./src/assets"),
+    },
+  },
+});
